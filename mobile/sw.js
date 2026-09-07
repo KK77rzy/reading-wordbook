1fc7122aaa56fc9ddf6f37bcc9e600198072f9b1
任俊 <rzy@renjundeMacBook-Pro.local>
2026-09-08 02:40:44 +0800

    Add hide all word details option

--- mobile/sw.js
+++ mobile/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "margin-sentences-v162";
+const CACHE = "margin-sentences-v163";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
