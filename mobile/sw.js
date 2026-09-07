ec45455b3210a5fe6ed1d0f65ddceea8f9f3ca38
任俊 <rzy@renjundeMacBook-Pro.local>
2026-09-07 23:32:28 +0800

    Align synonym descriptions

--- mobile/sw.js
+++ mobile/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "margin-sentences-v159";
+const CACHE = "margin-sentences-v160";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
